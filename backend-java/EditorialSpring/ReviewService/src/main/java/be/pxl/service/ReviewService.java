51c6c68461a3fbda74a89857e68a14b4a91f7f21
SanderCrijnsPXL <sander.crijns@student.pxl.be>
2025-01-02 12:33:54 +0100

    us-8

--- backend-java/EditorialSpring/ReviewService/src/main/java/be/pxl/service/ReviewService.java
+++ backend-java/EditorialSpring/ReviewService/src/main/java/be/pxl/service/ReviewService.java
@@ -6,9 +6,13 @@ import be.pxl.domain.request.RabbitPostRequest;
 import be.pxl.domain.response.PostResponse;
 import be.pxl.domain.response.RabbitPostResponse;
 import be.pxl.repository.ReviewRepository;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,6 +22,7 @@ import java.util.List;
 public class ReviewService implements IReviewService {
     private final ReviewRepository reviewRepository;
     private final RabbitTemplate rabbitTemplate;
+    private final JavaMailSender mailSender;
 
     @RabbitListener(queues = "post-to-review-queue")
     public void receivePost(RabbitPostRequest post) {
@@ -31,6 +36,8 @@ public class ReviewService implements IReviewService {
         reviewRepository.save(post);
 
         rabbitTemplate.convertAndSend("review-to-post-queue", mapPostToRabbitPostResponse(post));
+
+        sendEmail(post);
     }
 
     @Override
@@ -39,6 +46,8 @@ public class ReviewService implements IReviewService {
         post.setStatus(ReviewStatus.REJECTED);
         post.setComment(comment);
         reviewRepository.save(post);
+
+        sendEmail(post);
     }
 
     @Override
@@ -82,5 +91,25 @@ public class ReviewService implements IReviewService {
                 .id(post.getId())
                 .status(post.getStatus().toString())
                 .build();
+    }
+
+    private void sendEmail(Post post) {
+        String subject = "Post Review Status Update";
+        String recipient = "sander.crijns@student.pxl.be";
+        String message = String.format("Post with Title %d, ID %d has been %s.", post.getTitle(), post.getId(), post.getStatus().toString().toLowerCase());
+
+        try {
+            MimeMessage mail = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+
+            helper.setTo(recipient);
+            helper.setSubject(subject);
+            helper.setText(message, false);
+
+            mailSender.send(mail);
+            System.out.println("Email sent successfully to " + recipient);
+        } catch (MessagingException e) {
+            System.err.println("Failed to send email: " + e.getMessage());
+        }
     }
 }
